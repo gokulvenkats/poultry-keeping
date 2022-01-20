cd6b4d7d0d4ffe7677037d6c02a1ed7cd1e2c30a
gokulvenkats <gokul.venkatesan.gv@gmail.com>
2022-01-20 20:34:32 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poultry-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'poultry-keeping';
+}
